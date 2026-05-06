e93a1c0aca108d0966d394e871d5559f91f3f5b6
jqparnell <parnelldeplano@gmail.com>
2026-05-06 01:37:05 +0000

    made functions for input and letter count

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 // main.js
+const wordList = [];
+let wordCount = 0;
+let aCount=bCount=cCount=dCount=eCount=fCount=gCount=hCount=iCount=jCount=kCount=lCount=mCount=nCount=oCount=pCount=qCount=rCount=sCount=tCount=uCount=vCount=wCount=xCount=yCount=zCount = 0;
 
-console.log("Hello, World!");
+function saveInput() {
+    wordList.push(document.getElementById("enteredWord").value);
+    wordCount++;
+    updateLetterCount();
+    console.log("Input saved to variable:", wordList[wordCount-1]);
+    console.log("Word count:", wordCount);
+    console.log("Word list:", wordList);
+}
+function updateLetterCount() {
+    aPresentNum = wordList[wordCount-1].split('a').length - 1;
+    aCount = aCount+aPresentNum;
+    console.log("A's in word: ", aPresentNum);
+    console.log("Total A's: ", aCount);
+}
